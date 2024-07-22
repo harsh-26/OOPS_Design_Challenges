5d8dcd767e3066c770722d4cf3309544cc5cb3db
harsh-26 <harsh.shah2@morningstar.com>
2024-07-22 17:29:26 +0530

    Updated LeetCode Tasks

---
+++ SQL/LeetCode50Problems/1recyclable-and-low-fat-products.sql
@@ -0,0 +1,70 @@
+/*
+Table: Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key (column with unique values) for this table.
+low_fats is an ENUM (category) of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM (category) of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+ 
+
+Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
+
+*/
+
+-- Create the table if it doesn't exist
+-- SQL Server Script --
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Products' AND xtype='U')
+BEGIN
+    CREATE TABLE Products (
+        product_id INT PRIMARY KEY,
+        low_fats CHAR(1) CHECK (low_fats IN ('Y', 'N')),
+        recyclable CHAR(1) CHECK (recyclable IN ('Y', 'N'))
+    );
+END
+
+-- Truncate the table
+TRUNCATE TABLE Products;
+
+-- Insert data into the table
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (0, 'Y', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (1, 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (2, 'N', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (3, 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES (4, 'N', 'N');
+
+select product_id from products
+where low_fats = 'Y' AND recyclable = 'Y'
---
+++ SQL/LeetCode50Problems/2find-customer-referee.sql
@@ -0,0 +1,61 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+*/
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Customer' AND xtype='U')
+BEGIN
+    CREATE TABLE Customer (
+        id int, name varchar(25), referee_id int
+    );
+END
+
+Truncate table Customer
+insert into Customer (id, name, referee_id) values ('1', 'Will', NULL)
+insert into Customer (id, name, referee_id) values ('2', 'Jane', NULL)
+insert into Customer (id, name, referee_id) values ('3', 'Alex', '2')
+insert into Customer (id, name, referee_id) values ('4', 'Bill', NULL)
+insert into Customer (id, name, referee_id) values ('5', 'Zack', '1')
+insert into Customer (id, name, referee_id) values ('6', 'Mark', '2')
+
+select name from customer
+where referee_id != 2 OR referee_id is NULL
